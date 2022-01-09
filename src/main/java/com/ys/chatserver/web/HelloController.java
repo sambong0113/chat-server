f39cfd3062b6acb99435b4ed2f1983ba7005777b
yeonsuk Choi <sambong0113@gmail.com>
2022-01-09 20:44:28 +0900

    /hello API 처리 코드 작성

---
+++ src/main/java/com/ys/chatserver/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.ys.chatserver.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+}
